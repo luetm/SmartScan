c087aa84dc5555e143b337de133b300061f3a54c
Lukas Langenegger <lukas.langenegger@hexabit.ch>
2016-11-21 17:54:59 +0100

    Initial commit.

---
+++ ShowScan/Article.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ShowScan
+{
+    public class Article
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Stock { get; set; }
+        public decimal Price { get; set; }
+
+        public static Article ParseFromCsv(Context context, string csvLine)
+        {
+            try
+            {
+                if (csvLine == null)
+                    throw new ArgumentNullException(nameof(csvLine));
+
+                var parts = csvLine.Split(';');
+                if (parts.Length != 6)
+                    return null;
+
+                decimal price;
+                if (!decimal.TryParse(parts[4], NumberStyles.Any, CultureInfo.InvariantCulture, out price))
+                    return null;
+
+                var result = new Article
+                {
+                    Code = parts[0],
+                    Name = parts[1],
+                    Description = parts[2],
+                    Stock = parts[3],
+                    Price = price
+                };
+
+                return result;
+            }
+            catch (Exception err)
+            {
+                var toast = Toast.MakeText(context, $"{err.GetType().Name}: {err.Message}", ToastLength.Long);
+                toast.Show();
+                return null;
+            }
+        }
+    }
+}
---
+++ ShowScan/MainActivity.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using ZXing.Mobile;
+
+namespace ShowScan
+{
+    [Activity(Label = "ShowScan", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            MobileBarcodeScanner.Initialize(Application);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.ScanButton);
+
+            button.Click += async (s, e) =>
+            {
+                FindViewById<EditText>(Resource.Id.ProductName).Text = string.Empty;
+                FindViewById<EditText>(Resource.Id.Description).Text = string.Empty;
+                FindViewById<EditText>(Resource.Id.Stock).Text = string.Empty;
+                FindViewById<EditText>(Resource.Id.Price).Text = string.Empty;
+
+                var scanner = new MobileBarcodeScanner();
+                var result = await scanner.Scan();
+                var code = result.Text;
+
+                var path = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath, Android.OS.Environment.DirectoryDocuments, "Articles.csv");
+                var csvLines = File.ReadAllLines(path);
+                var article = csvLines
+                    .Select(l => Article.ParseFromCsv(ApplicationContext, l))
+                    .Where(a => a != null)
+                    .FirstOrDefault(x => x.Code == code);
+
+                if (article == null)
+                {
+                    var toast = Toast.MakeText(ApplicationContext, "No article found.", ToastLength.Long);
+                    toast.Show();
+                    
+                    return;
+                }
+
+                var notification = RingtoneManager.GetDefaultUri(RingtoneType.Notification);
+                Ringtone r = RingtoneManager.GetRingtone(ApplicationContext, notification);
+                r.Play();
+
+                FindViewById<EditText>(Resource.Id.ProductName).Text = article.Name;
+                FindViewById<EditText>(Resource.Id.Description).Text = article.Description;
+                FindViewById<EditText>(Resource.Id.Stock).Text = article.Stock;
+                FindViewById<EditText>(Resource.Id.Price).Text = article.Price.ToString("N");
+            };
+        }
+    }
+}
+
